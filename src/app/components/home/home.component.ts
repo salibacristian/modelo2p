326e4f71aea4f33ea2d3e22092e7a62cff926b5c
salibacristian <chriseze21@gmail.com>
2019-11-20 15:48:55 -0300

    export to pdf

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
-import {  Component, ViewChild, ElementRef } from '@angular/core';
-// import * as jspdf from 'jspdf';  
-
-
+import { Component, OnInit, ElementRef ,ViewChild} from '@angular/core';  
+import * as jspdf from 'jspdf'   
+import html2canvas from 'html2canvas';  
 
 @Component({
   selector: 'app-home',
@@ -10,15 +9,22 @@ import {  Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class HomeComponent {
 
-  // name = 'Angular';
-
-  // @ViewChild('content', {static: false}) content: ElementRef;
-
-  // makePdf() { 
-  //   let doc = new jspdf();
-  //   doc.addHTML(this.content.nativeElement, function() {
-  //      doc.save("obrz.pdf");
-  //   });
-  // }
+   public captureScreen()  
+  {  
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
+  }
 
 }
