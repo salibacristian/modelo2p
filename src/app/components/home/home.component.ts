1eaa70ed70f0f6b5455aca51e90c272bed6101be
salibacristian <chriseze21@gmail.com>
2019-11-20 18:11:39 -0300

    export to csv

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef ,ViewChild} from '@angular/core';  
-import * as jspdf from 'jspdf'   
-import html2canvas from 'html2canvas';  
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as jspdf from 'jspdf'
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-home',
@@ -9,22 +10,42 @@ import html2canvas from 'html2canvas';
 })
 export class HomeComponent {
 
-   public captureScreen()  
-  {  
-    var data = document.getElementById('contentToConvert');  
-    html2canvas(data).then(canvas => {  
+  array = [
+    { Column1: "Row 1", Column2: "Row 1", Column3: "Row 1" },
+    { Column1: "Row 2", Column2: "Row 2", Column3: "Row 2" },
+    { Column1: "Row 3", Column2: "Row 3", Column3: "Row 3" },
+    { Column1: "Row 4", Column2: "Row 4", Column3: "Row 4" },
+    { Column1: "Row 5", Column2: "Row 5", Column3: "Row 5" },
+    { Column1: "Row 6", Column2: "Row 6", Column3: "Row 6" }
+  ];
+
+  public captureScreen() {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
       // Few necessary setting options  
-      var imgWidth = 208;   
-      var pageHeight = 295;    
-      var imgHeight = canvas.height * imgWidth / canvas.width;  
-      var heightLeft = imgHeight;  
-  
-      const contentDataURL = canvas.toDataURL('image/png')  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
       let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
-      var position = 0;  
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
       pdf.save('MYPdf.pdf'); // Generated PDF   
-    });  
+    });
+  }
+
+  downloadFile() {
+    var data = this.array;
+    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    let csv = data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+    csv.unshift(header.join(','));
+    let csvArray = csv.join('\r\n');
+
+    var blob = new Blob([csvArray], { type: 'text/csv' })
+    saveAs(blob, "myFile.csv");
   }
 
 }
