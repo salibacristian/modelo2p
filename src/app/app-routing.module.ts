2f6e76f5aa29c19bb13acc7b6b9b713ec3b96c5f
salibacristian <chriseze21@gmail.com>
2019-11-21 09:48:03 -0300

    canactivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './helpers/auth.guard';
+import { AltaVehiculoComponent } from './components/alta-vehiculo/alta-vehiculo.component';
 
 
 const routes: Routes = [
    { path: '', redirectTo: 'home', pathMatch: 'full' },
-   {path: 'home' , component: HomeComponent/*, canActivate: [AuthGuard]*/},
+   {path: 'home' , component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'register' , component: RegisterComponent},
   {path: 'login' , component: LoginComponent},
+  {path: 'alta-vehiculo' , component: AltaVehiculoComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
